bf62a410a86287d97cbbbeba41eb2311c079a08d
Your Name <you@example.com>
2018-07-09 20:46:31 +0800

    update to zcj

--- app/home/search/search_urls.py
+++ app/home/search/search_urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from app.home.search import search_api
 
 urlpatterns = [
-    path('', search_api.search)
+    # 商品搜索
+    path(r'search/', search_api.search_goods, name='search_goods'),
+    path(r'index/', search_api.index, name='index'),
 ]
