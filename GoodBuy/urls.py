23a89a91cef2ee1869695e51e393d83c5f933cd1
SongJiaxin95 <1192567449@qq.com>
2018-07-12 11:38:33 +0800

    update to sjx

--- GoodBuy/urls.py
+++ GoodBuy/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from app import views
+from app.admin.admin_page import admin_page_api
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('verify_code/', views.verify_code),
@@ -25,6 +27,7 @@ urlpatterns = [
     path('user/', include('app.home.user.user_urls')),
     path('', include('app.home.goods.goods_urls')),
     path('admin_page/', include('app.admin.admin_page.admin_page_urls')),
+    path('admin/', admin_page_api.admin_page),
     path('admin_user_manage/', include('app.admin.admin_user_manage.admin_user_manage_urls')),
     path('goods_manage/', include('app.admin.goods_manage.goods_manage_urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
